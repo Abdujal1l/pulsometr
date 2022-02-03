242724067016760756ecb9513e2f3f7160013608
Abdujalil <olimjonov309@gmail.com>
2022-02-03 07:50:52 +0200

    animated

--- src/js/script.js
+++ src/js/script.js
@@ -43,7 +43,127 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
-}) ;
 
+    //modal
 
+    $('[data-modal=consultation]').on('click ', function(){
+        $('.overlay , #consultation' ).fadeIn();
+    });
 
+    $('.modal__close').on('click' , function(){
+      $('.overlay , #consultation , #thanks , #order').fadeOut('slow');
+    });
+
+    // $('.button_mini').on('click' , function(){
+    //   $('.overlay , #order').fadeIn('slow');
+    // });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order  .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay , #order').fadeIn('slow');
+      });
+    });
+
+    //valid
+    // $('.feed-form').validate({
+    //   debug: true
+    // });
+    // $('#consultation form').validate({
+    //   debug: true
+    // });
+    // $('#order form').validate({
+    //   debug: true,
+    //   rules: {
+    //     name: "required",
+    //     phone: "required",
+    //     email: {
+    //       required: true,
+    //       email: true
+    //     }
+    //   },
+    //   messages: {
+    //     errorClass: "error",
+    //     name: "пожалуйста введите свое имя",
+    //     phone: "пожалуйста введите свой телефон" , 
+    //     email: {
+    //       required: "подалуйста введите свой почтовый адрес",
+    //       email: "неправильно введен адрес почты name@domain.com"
+    //     }
+    //   }
+
+    // });
+    // $('#consultation-form').validate({
+    //   debug: true
+    // });
+    
+    function validateForms (form){
+      $(form).validate({
+        debug: true,
+        rules: {
+          name: "required",
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          errorClass: "error",
+          name: "пожалуйста введите свое имя",
+          phone: "пожалуйста введите свой телефон" , 
+          email: {
+            required: "подалуйста введите свой почтовый адрес",
+            email: "неправильно введен адрес почты name@domain.com"
+          }
+        } 
+  
+      });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7(999)999999");
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+
+      if (!$(this).valid()){
+          return;
+      }
+      
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+          $(form).trigger('reset');
+      });
+    });
+
+    //smooth scroll and pageup
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 1600) {
+          $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    // $("a[href^='#']").click(function(){
+    //   var _href = $(this).attr("href");
+    //   $("html, body").animate({scrollTop: $(_href).offset().top+"px" });
+    //   return false;
+    // });
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
+
+  });
